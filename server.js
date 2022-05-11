0473544d6ceda21b80dcd6507aa4c520854ed64a
Beimy <arbm159357@gmail.com>
2022-05-11 14:05:27 -0400

    installed dotenv, connected our database to express server

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
+// EXPRESS IMPORTS
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// MySQL IMPORTS
+const mysql = require('mysql2');
+// DOTENV IMPORTS
+require('dotenv').config();
 
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: process.env.host,
+        // Your MySQL username
+        user: process.env.user,
+        // Your MySQL password
+        password: process.env.password,
+        database: process.env.database
+    },
+
+    console.log('Connected to the election database.')
+);
+
+// Database Queries
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Express ROUTES
 
